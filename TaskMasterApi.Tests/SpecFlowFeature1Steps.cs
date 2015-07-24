f9e31572eb0223e672d8384a3256dc5ca8cf9e7d
kylebatcdhdotcom <kyleb@cdh.com>
2015-07-24 11:59:36 -0400

    Added specflow

---
+++ TaskMasterApi.Tests/SpecFlowFeature1Steps.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TaskMasterApi.Tests
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private int number1 { get; set; }
+        private int number2 { get; set; }
+        private int result { get; set; }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            number1 = p0;
+        }
+        
+        [Given(@"I have entered (.*) into the calculator as well")]
+        public void GivenIHaveEnteredIntoTheCalculatorAsWell(int p0)
+        {
+            number2 = p0;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = number1 + number2;
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, result, "Result is not correct");
+        }
+    }
+}
